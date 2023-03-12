c4b6c14ff4b67b926a08e302b3aa014f0eb7a7da
Manoj Cherukumalla <101888400+ManojCherukumalla9@users.noreply.github.com>
2023-03-12 10:06:00 +0530

    Add files via upload

---
+++ ja.js
@@ -0,0 +1,26 @@
+const readMoreBtn = document.querySelector(".read-more-btn");
+const text = document.querySelector(".text");
+
+readMoreBtn.addEventListener("click", (e) => {
+  text.classList.toggle("show-more");
+  if (readMoreBtn.innerText === "Show More") {
+    readMoreBtn.innerText = "Show Less";
+  } else {
+    readMoreBtn.innerText = "Show More";
+  }
+});
+
+// *********************
+// This Code is for only the floating card in right bottom corner
+// **********************
+
+const touchButton = document.querySelector(".float-text");
+const card = document.querySelector(".float-card-info");
+const close = document.querySelector(".gg-close-r");
+
+touchButton.addEventListener("click", moveCard);
+close.addEventListener("click", moveCard);
+
+function moveCard() {
+  card.classList.toggle("active");
+}
